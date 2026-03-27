660e2e9201c73528399e5c074e885d43875edb76
finoz <finoz.webdev@gmail.com>
2026-03-27 16:18:10 +0100

    version update

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 
 // ── Auto-updater ──────────────────────────────────────────────────────────────
 
-define( 'FNZ_THEME_VERSION',     '1.0.7' );
+define( 'FNZ_THEME_VERSION',     '1.0.8' );
 define( 'FNZ_THEME_SLUG',        'fnz-wpt-milanscore' );
 define( 'FNZ_THEME_GITHUB_REPO', 'finoz/wpt-milanscore' );
 
